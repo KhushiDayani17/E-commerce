1e73f10c2ef2e608bd95d14e885fafb997b450bd
Mansi Dayani <mansidayani732@gmail.com>
2023-11-27 14:44:42 +0530

    product detail page

--- components/HomePage/Product.tsx
+++ components/HomePage/Product.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import AuthApiServices from "../../helpers/apis/authProductApiServices";
 import { IProductPayload } from "../../@types/apis/IProduct";
+import Link from "next/link";
 
 const Product = () => {
   const [products, setProducts] = useState<IProductPayload[]>([]);
@@ -32,21 +33,21 @@ const Product = () => {
               key={product.id}
               className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md"
             >
-              <a
+              <Link
+                href="/product_detail"
                 className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl flex justify-center"
-                href="#"
               >
                 <img src={product.image} />
                 <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
                   {/* {product.discountPercent}% OFF */}
                 </span>
-              </a>
+              </Link>
               <div className="mt-4 px-5 pb-5">
-                <a href="#">
+                <Link href="/product_detail">
                   <h5 className="text-xl tracking-tight text-slate-900">
                     {product.title}
                   </h5>
-                </a>
+                </Link>
                 <div className="mt-2 mb-5 flex items-center justify-between">
                   <p>
                     <span className="text-3xl font-bold text-slate-900">
---
+++ pages/product_detail.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import Product from "../components/Product/Product";
+
+const product_detail = () => {
+  return (
+    <div>
+        <Header/>
+        <Product/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default product_detail
