ac2b699a9f1e2b1b4e451513ae55b968220034a0
Mansi Dayani <mansidayani732@gmail.com>
2023-12-01 00:03:53 +0530

    header responsive

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -272,205 +272,141 @@ const beautyCategories = [
     items: ["Perfume", "Deodorant", "Body Mist"],
   },
 ];
+const NavBarLink = ({ href, text, submenu }: any) => {
+  return (
+    <li className="hoverable">
+      <a
+        href={href}
+        className="relative block py-6 px-4 lg:p-6 text-sm lg:text-base font-bold"
+      >
+        {text}
+      </a>
+      {submenu && (
+        <div className="p-6 mega-menu mb-16 sm:mb-0 shadow-xl bg-white">
+          <div className="grid grid-cols-4 gap-4">
+            {submenu.map((category: any, index: number) => (
+              <div key={index}>
+                <h3 className="text-lg font-semibold mb-2">{category.title}</h3>
+                <ul>
+                  {category.items.map((item: string, itemIndex: number) => (
+                    <li
+                      key={itemIndex}
+                      className="hover:text-black hover:font-semibold cursor-pointer mb-2"
+                    >
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </li>
+  );
+};
+
 const Header = () => {
   const cartItem = useSelector(selectCart);
   const count = cartItem.items.length;
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
-      <nav className="flex relative bg-white border-b-2 border-gray-300 text-gray-900 w-full">
-        <div className="container mx-auto flex justify-between items-center">
-          <div className="relative block p-4 lg:p-6 text-xl text-teal-600 font-bold">
+      <nav className={`${menuOpen ? "absolute z-10 bg-white w-full sm:w-1/2 right-0" : "flex relative bg-white border-b-2 border-gray-300 text-gray-900 w-full"}`}>
+        <div className={`${menuOpen ? "container mx-auto flex flex-col items-center" : "container mx-auto flex justify-between items-center"}`}>
+          <div className={"relative block p-4 lg:p-6 text-xl text-teal-600 font-bold"}>
             <a href="#" className="text-teal-900">
               Shopsurf
             </a>
           </div>
-          <ul className="flex">
-            <li className="hoverable">
-              <a
-                href="#"
-                className="relative block py-6 px-4 lg:p-6 text-sm lg:text-base font-bold"
-              >
-                Men
-              </a>
-              <div className="p-6 mega-menu mb-16 sm:mb-0 shadow-xl bg-white">
-                <div className="grid grid-cols-4 gap-4">
-                  {MenCategories.map((category, index) => (
-                    <div key={index}>
-                      <h3 className="text-lg font-semibold mb-2">
-                        {category.title}
-                      </h3>
-                      <ul>
-                        {category.items.map((item, itemIndex) => (
-                          <li
-                            key={itemIndex}
-                            className="hover:text-black hover:font-semibold cursor-pointer mb-2"
-                          >
-                            {item}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
+          <div className={`${menuOpen ? "flex justify-end sm:gap-32 gap-10 m-4" : ""}`}>
+            <ul
+              className={`${menuOpen ? "flex flex-col items-center" : "hidden"
+                } lg:flex`}
+            >
+              <NavBarLink href="#" text="Men" submenu={MenCategories} />
+              <NavBarLink href="#" text="Women" submenu={WomenCategories} />
+              <NavBarLink href="#" text="Kids" submenu={KidsCategories} />
+              <NavBarLink href="#" text="Furniture" submenu={furnitureCategories} />
+              <NavBarLink href="#" text="Beauty" submenu={beautyCategories} />
+              <div className="flex gap-6 relative block p-4 lg:p-6 text-xl font-bold">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                    />
+                  </svg>
+                </div>
+                <div>
+                  <Link href="/cart">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                      />
+                    </svg>
+                    {count > 0 && (
+                      <div className="absolute top-0 right-1 bg-gray-300 text-white text-sm rounded-full w-6 h-6 flex items-center justify-center">
+                        {count}
+                      </div>
+                    )}
+                  </Link>
                 </div>
               </div>
-            </li>
-            <li className="hoverable">
-              <a
-                href="#"
-                className="relative block py-6 px-4 lg:p-6 text-sm lg:text-base font-bold"
+            </ul>
+            <div className="lg:hidden">
+              <button
+                className="text-gray-900 focus:outline-none"
+                onClick={() => setMenuOpen(!menuOpen)}
               >
-                Women
-              </a>
-              <div className="p-6 mega-menu mb-16 sm:mb-0 shadow-xl bg-white">
-                <div className="grid grid-cols-4 gap-4">
-                  {WomenCategories.map((category, index) => (
-                    <div key={index}>
-                      <h3 className="text-lg font-semibold mb-2">
-                        {category.title}
-                      </h3>
-                      <ul>
-                        {category.items.map((item, itemIndex) => (
-                          <li
-                            key={itemIndex}
-                            className="hover:text-black hover:font-semibold cursor-pointer mb-2"
-                          >
-                            {item}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </li>
-            <li className="hoverable">
-              <a
-                href="#"
-                className="relative block py-6 px-4 lg:p-6 text-sm lg:text-base font-bold"
-              >
-                Kids
-              </a>
-              <div className="p-6 mega-menu mb-16 sm:mb-0 shadow-xl bg-white">
-                <div className="grid grid-cols-4 gap-4">
-                  {KidsCategories.map((category, index) => (
-                    <div key={index}>
-                      <h3 className="text-lg font-semibold mb-2">
-                        {category.title}
-                      </h3>
-                      <ul>
-                        {category.items.map((item, i) => (
-                          <li
-                            key={i}
-                            className="hover:text-black hover:font-semibold cursor-pointer mb-2"
-                          >
-                            {item}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </li>
-            <li className="hoverable">
-              <a
-                href="#"
-                className="relative block py-6 px-4 lg:p-6 text-sm lg:text-base font-bold"
-              >
-                Furniture
-              </a>
-              <div className="p-6 mega-menu mb-16 sm:mb-0 shadow-xl bg-white">
-                <div className="grid grid-cols-4 gap-4">
-                  {furnitureCategories.map((category, index) => (
-                    <div key={index}>
-                      <h3 className="text-lg font-semibold mb-2">
-                        {category.title}
-                      </h3>
-                      <ul>
-                        {category.items.map((item, i) => (
-                          <li
-                            key={i}
-                            className="hover:text-black hover:font-semibold cursor-pointer mb-2"
-                          >
-                            {item}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </li>
-            <li className="hoverable">
-              <a
-                href="#"
-                className="relative block py-6 px-4 lg:p-6 text-sm lg:text-base font-bold"
-              >
-                Beauty
-              </a>
-              <div className="p-6 mega-menu mb-16 sm:mb-0 shadow-xl bg-white">
-                <div className="grid grid-cols-4 gap-4">
-                  {beautyCategories.map((category, index) => (
-                    <div key={index}>
-                      <h3 className="text-lg font-semibold mb-2">
-                        {category.title}
-                      </h3>
-                      <ul>
-                        {category.items.map((item, i) => (
-                          <li
-                            key={i}
-                            className="hover:text-black hover:font-semibold cursor-pointer mb-2"
-                          >
-                            {item}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </li>
-          </ul>
-          <div className="flex gap-6 relative block p-4 lg:p-6 text-xl font-bold">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                />
-              </svg>
-            </div>
-            <div>
-              <Link href="/cart">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                  />
-                </svg>
-                {count > 0 && (
-                  <div className="absolute top-0 right-1 bg-gray-300 text-white text-sm rounded-full w-6 h-6 flex items-center justify-center">
-                    {count}
-                  </div>
+                {menuOpen ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  </svg>
                 )}
-              </Link>
+              </button>
             </div>
           </div>
         </div>
